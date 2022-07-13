6df85d8352e256a7d93f9ebc87b583320bb12ab6
Danica Yener <ny21037@googlemail.com>
2022-07-13 17:54:33 +0200

    function

---
+++ solution.js
@@ -0,0 +1,39 @@
+function multiply(a, b) {
+  console.log("the result is", a * b);
+}
+multiply(2, 4);
+
+//2
+
+const secondQues = function (a, b) {
+console.log(a * b);
+}
+secondQues (5, 6);
+//3
+
+const thirdQes =(a, b)=>{
+    let result =(a * b);
+    return result;
+}
+console.log(thirdQes(3, 5));
+
+const arrowValue = (a, b) =>{
+return a * b;
+}
+console.log(arrowValue(8, 10));
+//4 
+
+function danica(a, b) {
+    console.log(a % b);
+}
+danica(7, 3)
+
+const alan = function(a, b){
+    return a % b;
+}
+console.log(alan(7,9));
+
+const rabi = (a, b) =>{
+    console.log(a % b);
+}
+rabi(6,4);
